979fad85a2bdb4305e6779ebf20ae892730b5dc3
Utsr008 <utsr005@gmail.com>
2026-02-03 15:21:00 +0530

    second commit

--- file.js
+++ file.js
@@ -1,5 +1,18 @@
-const add = (a, b) => a + b;
-const multiply = (a, b) => a * b;
+//const add = (a, b) => a + b;
+//const multiply = (a, b) => a * b;
 
-export default add;
-export { multiply };
+//export default add;
+//export { multiply };
+
+import fs from "fs";
+//const output =fs.readFileSync("file.txt","utf-8");
+//console.log(output);
+//fs.writeFileSync("file.txt","File read successfully");
+//fs.appendFileSync("file.txt","\nAppended successfully");
+fs.readFile("file.txt","utf-8",(err,output)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(output);
+});
+console.log("ended");
