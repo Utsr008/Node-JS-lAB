f7806d34927ad51305fca2a8f2df7a243ff737fe
Utsr008 <utsr005@gmail.com>
2026-03-31 15:50:53 +0530

    Refactor views with includes and update app routing

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 const express=require('express');
 const app=express();
 
-const authRoutes = require("./authroutes");
-const verifyToken = require('./middleware/authMiddleware');
+app.set("view engine","ejs");
 
-app.use("/auth", authRoutes);
+const homePageContent={
+    header:{title:"GLA header"},
+    footer:{title:"GLA footer"},
+    content:{title:"GLA content"},
+    data:{
+        universityName:"GLA University Mathura",
+        students:[
+            { name: "Utkarsh",
+                age:21,
+                cgpa:8.5
+            }
+        ]
+    }
+};
+
+app.get("/",(req,res)=>{
+    res.render("header",homePageContent.header);
+});
+
+app.get("/footer",(req,res)=>{
+    res.render("footer",homePageContent.footer);
+});
+
+app.get("/content",(req,res)=>{
+    res.render("content",homePageContent.content);
+});
+
+
+
+//const authRoutes = require("./authroutes");
+//const verifyToken = require('./middleware/authMiddleware');
+
+//app.use("/auth", authRoutes);
 
 // Protected route that requires a valid JWT
-app.get("/protected", verifyToken, (req, res) => {
-    res.json({
-        message: "You have accessed a protected route successfully!",
-        userData: req.user
-    });
-});
+//app.get("/protected", verifyToken, (req, res) => {
+  //  res.json({
+    //    message: "You have accessed a protected route successfully!",
+      //  userData: req.user
+    //});
+//});
 
 // const userRoutes=require("./routes/userRoutes");
 // const productRoutes=require("./routes/productRoutes");
